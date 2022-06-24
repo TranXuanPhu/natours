8dcdcc403db99fffd19dcd0a1dcb2795fd1d4af3
Xuân Phú Trần <xuanphu1997@gmail.com>
2022-06-24 15:34:39 +0700

    Init project

---
+++ index.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+//config in process.env
+dotenv.config({ path: './config.env' });
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World !');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
